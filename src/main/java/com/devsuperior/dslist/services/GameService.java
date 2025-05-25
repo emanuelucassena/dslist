9e3c50a8be26c1ef0e9f91c1734ee51a8b4be93a
Emanuel <lucasemanuelsena@outlook.com>
2025-05-25 11:52:06 -0300

    get by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ public class GameService {
     @Autowired // Injeta automaticamente uma instância do GameRepository (injeção de dependência)
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     // Método que busca todos os jogos e retorna apenas os dados mínimos via DTO
     public List<GameMinDTO> findAll() {
         // Busca todos os jogos no banco de dados
@@ -24,5 +27,11 @@ public class GameService {
 
         // Retorna a lista de DTOs
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findbyId(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
